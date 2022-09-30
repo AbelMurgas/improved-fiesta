7baf75bf58daef2beb6d9d2e510d6f2a89ba1140
AbelMurgas <abelmurgasdev@gmail.com>
2022-09-29 23:44:29 -0500

    complete project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Title from './component/Title'
+import DesignerSignature from './component/DesignerSignature'
+import Triangle from './component/Triangle'
+import HeartIcon from './component/HeartIcon'
+import Guests from './component/Guests'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <Title />
+        <Guests />
+      </div>
+      <DesignerSignature />
+      <Triangle />
+      <HeartIcon />
     </div>
   );
 }
---
+++ my-app/src/component/Title.js
@@ -0,0 +1,9 @@
+export default function Title(){
+ return (
+  <div className="d-flex flex-column title text-center">
+   <span className="text-100">Mancolor</span>
+   <hr className="line"/>
+   <span className="text-50 text-black">descubre tu color</span>
+  </div>
+ );
+}
